2e4e52e5f082df755b3b6d93cbee5a870f82ecde
Lau <dlaurenap@gmail.com>
2025-10-28 19:39:59 -0300

    ayudas

--- ratings-counter.py
+++ ratings-counter.py
@@ -1,13 +1,49 @@
-from pyspark import SparkConf, SparkContext
+import argparse
 import collections
 
-conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
-sc = SparkContext(conf = conf)
+from pyspark import SparkConf, SparkContext
 
-lines = sc.textFile("file:///SparkCourse/ml-100k/u.data")
-ratings = lines.map(lambda x: x.split()[2])
-result = ratings.countByValue()
 
-sortedResults = collections.OrderedDict(sorted(result.items()))
-for key, value in sortedResults.items():
-    print("%s %i" % (key, value))
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Compute the histogram of rating values from the MovieLens 100K dataset."
+    )
+    parser.add_argument(
+        "input_path",
+        nargs="?",
+        default="file:///opt/spark/work-dir/ml-100k/u.data",
+        help="Path to the ratings data file (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--master",
+        default="local[*]",
+        help="Spark master URL to use (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--app-name",
+        default="RatingsHistogram",
+        help="Name to assign to the Spark application (default: %(default)s)",
+    )
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = parse_args()
+
+    conf = SparkConf().setMaster(args.master).setAppName(args.app_name)
+    sc = SparkContext(conf=conf)
+
+    try:
+        lines = sc.textFile(args.input_path)
+        ratings = lines.map(lambda x: x.split()[2])
+        result = ratings.countByValue()
+
+        sorted_results = collections.OrderedDict(sorted(result.items()))
+        for key, value in sorted_results.items():
+            print(f"{key} {value}")
+    finally:
+        sc.stop()
+
+
+if __name__ == "__main__":
+    main()
